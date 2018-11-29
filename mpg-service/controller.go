1a45e87ab0573e77da1097a6e4a38bfaba4db3d0
Loic Geffroy <loic.geffroy@masagroup.net>
2018-11-29 16:19:38 +0000

    [mpg-service] Extract players information.

--- mpg-service/controller.go
+++ mpg-service/controller.go
@@ -1,29 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-func getPlayers(league, key string) (string, error) {
+type Player struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+type Data struct {
+	Players []*Player `json:"players"`
+}
+
+type PlayersInfo struct {
+	Data *Data `json:"data"`
+}
+
+func getPlayers(league, key string) ([]*Player, error) {
 	client := &http.Client{}
 
 	url := fmt.Sprintf("https://api.monpetitgazon.com/league/%s/coach", league)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	req.Header.Set("Authorization", key)
 	rpy, err := client.Do(req)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer rpy.Body.Close()
+
 	body, err := ioutil.ReadAll(rpy.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(body), nil
+
+	var info PlayersInfo
+	err = json.Unmarshal(body, &info)
+	if err != nil {
+		return nil, err
+	}
+	return info.Data.Players, nil
 }
